b989f3b5846899b293f67df371298b6bbc7fc2a7
choco <adrian@.com>
2025-12-04 14:21:15 +0100

    sauvegarde toi pls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
-"use client";
-import { AlignEndVertical } from "lucide-react";
-import IconButton from "../components/Buttons/IconButton";
+import Card from "../components/Cards/Card"
 
 export default function Home() {
-  return <div className="size-full justify-center items-center"></div>;
+  return <>
+  <Card
+  type="iconCard"
+  bgColor="bg-slate-800"
+  dimension={{ minheigh: "min-h-40", minwidth: "min-w-64" }}
+  icon={{ icon: "🚀", iconPos: "left" }}
+  txt={{
+    title: "Card",
+    children: "Some description here",
+    textColor: "text-white",
+    titleColor: "text-blue-400",
+    textsize: "text-xl",
+    titlesize: "text-xxl",
+  }}
+>
+</Card>
+</>;
 }
